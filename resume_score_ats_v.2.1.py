36e28d51aaf2f86b788deaa9ec2cff616d283927
noerbarry <noerbarry@gmail.com>
2023-06-05 23:19:10 +0700

    Update resume_score_ats_v.2.1.py

--- resume_score_ats_v.2.1.py
+++ resume_score_ats_v.2.1.py
@@ -103,7 +103,15 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-# File upload form for resume
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html# File upload form for resume
 uploaded_file = st.file_uploader("Upload LinkedIn resume file", type=["pdf", "docx", "doc", "pptx", "ppt"])
 
 # Validation button
