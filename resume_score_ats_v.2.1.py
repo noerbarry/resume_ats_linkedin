192bd45f99f6a8ddd320d544f5947e39a09dfcc9
noerbarry <noerbarry@gmail.com>
2023-06-05 17:43:18 +0700

    Update resume_score_ats_v.2.1.py

--- resume_score_ats_v.2.1.py
+++ resume_score_ats_v.2.1.py
@@ -94,15 +94,14 @@ def validate_resume_en(file_path):
 st.title("Linkedin Resume Validation using ATS System method")
 st.write('Noer Barrihadianto')
 
-st.markdown(
-    """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 # File upload form for resume
 uploaded_file = st.file_uploader("Upload LinkedIn resume file", type=["pdf", "docx", "doc", "pptx", "ppt"])
 
