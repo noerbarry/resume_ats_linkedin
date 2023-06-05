89700256a32ce41269bb5510529526b7432f26a4
noerbarry <noerbarry@gmail.com>
2023-06-05 17:58:31 +0700

    Update resume_score_ats_v.2.1.py

--- resume_score_ats_v.2.1.py
+++ resume_score_ats_v.2.1.py
@@ -95,11 +95,20 @@ st.title("Linkedin Resume Validation using ATS System method")
 st.write('Noer Barrihadianto')
 
 hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
+        footer {
+	
+	visibility: hidden;
+	
+	}
+footer:after {
+	content:'goodbye'; 
+	visibility: visible;
+	display: block;
+	position: relative;
+	#background-color: red;
+	padding: 5px;
+	top: 2px;
+}
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 # File upload form for resume
